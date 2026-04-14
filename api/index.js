4b27ca2e6dd2730c76eb32cc367cff7c052fa8dd
furrysachin <sachinkumark674@gmail.com>
2026-04-14 19:30:21 +0530

    Adjust Vercel build and API rewrites

--- api/index.js
+++ api/index.js
@@ -1,1 +1,17 @@
-module.exports = require('../task-api/src/app');
+const app = require('../task-api/src/app');
+
+module.exports = (req, res) => {
+  const host = req.headers.host || 'localhost';
+  const requestUrl = new URL(req.url, `http://${host}`);
+  const rewrittenPath = requestUrl.searchParams.get('path');
+
+  if (rewrittenPath) {
+    const params = new URLSearchParams(requestUrl.searchParams);
+    params.delete('path');
+    req.url = `/${rewrittenPath}${params.toString() ? `?${params.toString()}` : ''}`;
+  } else if (requestUrl.pathname === '/api') {
+    req.url = '/';
+  }
+
+  return app(req, res);
+};
